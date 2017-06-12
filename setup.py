1ca7732d9cc4060c43e42efddc6593ff9c32b4f9
Joshua Aduol <jna29@cam.ac.uk>
2017-06-12 21:52:07 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'SymGP',
+  packages = ['SymGP'], # this must be the same as the name above
+  version = '1.0.0',
+  description = 'A symbolic algebra library for multivariate Gaussian and Gaussian process probabilistic models',
+  author = 'Joshua Aduol',
+  author_email = 'jaduol@yahoo.co.uk',
+  url = 'https://github.com/jna29/SymGP/', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
